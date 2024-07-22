3248e6610b31ba69c470ad176c11581605d6ad70
Mouzone <sunnyliu010@gmail.com>
2024-07-22 12:18:12 -0400

    Remove correct task upon selecting checkbox

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ const form = document.querySelector("form")
 const new_task_name = document.querySelector("input#task-name")
 const new_task_due_date = document.querySelector("input#task-due-date")
 const content_area = document.getElementById("tasks")
-let task_id = 0
+let task_id = 1
 
 form.addEventListener("submit", event => {
-    // todo: figure out preventDefault
     // todo: figure out how to reset form
-    event.currentTarget.preventDefault()
+    event.preventDefault()
 
     const new_task_card = document.createElement("div")
     new_task_card.classList.add("task")
+    new_task_card.dataset.id = `${task_id}`
 
     const new_task_text = document.createElement("div")
     new_task_text.classList.add("text")
@@ -27,10 +27,11 @@ form.addEventListener("submit", event => {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.id = "task" + task_id;
-    checkbox.name = name;
+    checkbox.name = "task" + task_id;
+    checkbox.addEventListener("click", handleClick)
 
     const label = document.createElement('label');
-    checkbox.id = "task" + task_id;
+    label.setAttribute("for", "task" + task_id)
 
     new_task_text.appendChild(new_task_header)
     new_task_text.appendChild(new_task_p)
@@ -41,3 +42,10 @@ form.addEventListener("submit", event => {
     task_id++
     form.reset()
 })
+
+function handleClick(event){
+    const checkbox = event.currentTarget
+    const id_of_card = checkbox.id.slice(4)
+    const card_to_remove = document.querySelector(`div.task[data-id='${id_of_card}']`)
+    content_area.removeChild(card_to_remove)
+}
